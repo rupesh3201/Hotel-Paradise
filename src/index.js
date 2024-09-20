c5b68d57fc2b5d3e662cb2704b2a74cc8ea43208
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-21 01:03:51 +0530

    createBrowserRouter,RouterProvider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import ReactDom from "react-dom/client";
+import  { createBrowserRouter,RouterProvider}  from 'react-router-dom';
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(<h1> Hii</h1>);
