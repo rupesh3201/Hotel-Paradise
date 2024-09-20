df42e74d8ce538ad2c8d9a542f29e88f6308a1ea
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-21 02:04:02 +0530

    destructured g

--- src/Component/Foodcard/Foodcard.js
+++ src/Component/Foodcard/Foodcard.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './../../Css/Foodcard.css'
 function Foodcard(props) {
+    const {title,description} = props
   return (
-    <div><h1>{props.title}</h1>
-    <p>{props.description}</p>
+    <div><h1>{title}</h1>
+    <p>{description}</p>
     </div>
   )
 }
