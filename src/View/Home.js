7c838de91fee2509f5bd6a5c96d2922cb643fc97
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-21 01:27:54 +0530

    Craeted hone and about

---
+++ src/View/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div><h1>Hotel Paradise </h1></div>
+  )
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import ReactDom from "react-dom/client";
+import React from "react";
 import  { createBrowserRouter,RouterProvider}  from 'react-router-dom';
+import Home from "./View/Home";
+import About from "./View/About";
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<h1> Hii</h1>);
+const router =     createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+
+  },
+  {
+    path: 'About',
+    element: <About/>
+  }
+])
+root.render(<RouterProvider router = {router}/>);
