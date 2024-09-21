9c616c61e33eda22047268680a877c1c9d082865
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-22 01:28:14 +0530

    Added biryani

--- src/Component/Foodcard/Foodcard.js
+++ src/Component/Foodcard/Foodcard.js
@@ -2,16 +2,17 @@ import React from 'react'
 import './../../Css/Foodcard.css'
 
 import nonveg from './../../Assets/nonveg.png'
-
 import veg from './../../Assets/veg.png'
 
-function Foodcard({title, description ,isveg}) {
+
+function Foodcard({title, description ,isveg, imgurl}) {
     
   return (
     <div className='foodcard'>
+    <img src={imgurl} alt='img'  className='food-item-img'/>
       <h1 className='foodcard-title'>{title}</h1>
-    <p>{description}</p>
-    <img src={isveg ? veg : nonveg } className='food-card-icon' />
+    <p className='dis'>{description}</p>
+    <img src={isveg ? veg : nonveg } className='food-card-icon' alt='img' />
     </div>
   )
 }
--- src/View/Home.js
+++ src/View/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Foodcard from '../Component/Foodcard/Foodcard'
+
+import Biryani from './../Assets/Food_images/Biryani.jpg'
 function Home() {
   return (
     <div>
       <h1>Hotel Paradise </h1>
-      <Foodcard title="Delicious Pizza" description="A delightful cheesy pizza with fresh toppings!" isveg={true}/>
+
+      <Foodcard imgurl={Biryani}  title="Chikan Biryani" description=" Biryani is a spiced mix of meat and rice, traditionally cooked over an open fire in a leather pot." isveg={false}/>
       <Foodcard title="Tasty Burger" description="A juicy beef burger with crispy fries. " isveg={false} />
     </div>
   );
