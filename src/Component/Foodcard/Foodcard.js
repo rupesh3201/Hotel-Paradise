7dee98b66ae8ab61e458cb685cea82e215ac8723
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-21 19:30:12 +0530

    Dealing with css

--- src/Component/Foodcard/Foodcard.js
+++ src/Component/Foodcard/Foodcard.js
@@ -3,7 +3,8 @@ import './../../Css/Foodcard.css'
 function Foodcard({title, description}) {
     
   return (
-    <div className='foodcard'><h1>{title}</h1>
+    <div className='foodcard'>
+      <h1 className='foodcard-title'>{title}</h1>
     <p>{description}</p>
     </div>
   )
