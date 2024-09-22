be0f8a4e1006203aed1b2e8ab8741376138516e0
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-22 23:34:20 +0530

    added images

--- src/View/Home.js
+++ src/View/Home.js
@@ -8,6 +8,8 @@ import pannir from './../Assets/Food_images/panner.jpg'
 import dosa from './../Assets/Food_images/Masaladosa.jpg'
 import crab from './../Assets/Food_images/curry-7249247_1280.jpg'
 import crabcuryy from './../Assets/Food_images/maxresdefault.jpg'
+import eggs from './../Assets/Food_images/eggs.jpg'
+import fish from './../Assets/Food_images/fish.jpg'
 function Home() {
   return (
     <div>
@@ -22,8 +24,8 @@ function Home() {
       <Foodcard imgurl={crabcuryy} title="Crab Curry " description="Crab curry A healthy food that is a good source of protein.  " isveg={false} />
       <Foodcard imgurl={dosa}  title="Masala Dosa" description="Masala Dosa is one of the most popular South Indian breakfast dishes served in restaurants and tiffin centres.  " isveg={true} />
       <Foodcard imgurl={crab}  title="Kaju Masala Curry " description="Kaju Masala Curry is a rich, creamy curry made with cashews and a blend of aromatic spices . " isveg={true} />
-      <Foodcard imgurl={paratha}  title="Egg Curry" description="Egg curry is a comforting Indian dish of curried eggs. It is basically made with hard boiled eggs, onions, tomatoes, whole & ground spices and herbs. " isveg={false} />
-      <Foodcard imgurl={paratha}  title="Fish curry" description="A savory dish made with paanch phoran seeds and a pinch of red chili spice.  " isveg={true} />
+      <Foodcard imgurl={eggs}  title="Egg Curry" description="Egg curry is a comforting Indian dish of curried eggs. It is basically made with hard boiled eggs, onions, tomatoes, whole & ground spices and herbs. " isveg={false} />
+      <Foodcard imgurl={fish}  title="Fish curry" description="A savory dish made with paanch phoran seeds and a pinch of red chili spice.  " isveg={false} />
 
       </div>
     </div>
